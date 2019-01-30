7d52ef4f0960b7d258e51fff99ca8d51aa8d7a53
Guillermo Pineda <pinedag395@gmail.com>
2019-01-29 18:35:03 -0800

    Created Sign up page

--- src/App.js
+++ src/App.js
@@ -1,28 +1,62 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ReactDOM from 'react-dom';
 
-class App extends Component {
+class HomePage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      phoneNum: '',
+      schedule: ''
+    }
+
+    this.handleNameChange = this.handleNameChange.bind(this);
+    this.handlePhoneChange = this.handlePhoneChange.bind(this);
+    this.handleScheduleChange = this.handleScheduleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleNameChange(event) {
+    this.setState({name: event.target.value});
+  }
+
+  handlePhoneChange(event) {
+    this.setState({phoneNum: event.target.value});
+  }
+
+  handleScheduleChange(event) {
+    this.setState({schedule: event.target.value});
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+
+  }
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+    return(
+      <form onSubmit={this.handleSubmit}>
+        <label>Name:
+          <input type="text" value={this.state.name} onChange={this.handleNameChange}/>
+        </label>
+        <br/>
+        <label>Phone Number:
+          <input type="text" value={this.state.phoneNum} onChange={this.handlePhoneChange}/>
+        </label>
+        <br />
+        <label>Frequency:
+        <select value={this.state.schedule} onChange={this.handleScheduleChange}>
+          <option selected value="Daily">Daily</option>
+          <option value="Weekly">Weekly</option>
+          <option value="Monthly">Monthly</option>
+        </select>
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
     );
   }
 }
 
-export default App;
+ReactDOM.render(<HomePage />, document.getElementById('root'))
+
+export default HomePage;
